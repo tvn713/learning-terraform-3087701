bff268f7be7fe3e63134e19ecc9c405c420bd560
tvn713 <tuan_nguyen@epam.com>
2025-04-02 13:54:48 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,8 +17,26 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
-
+  subnets            = module.blog_vpc.public_subnets[0]
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+ 
+  name = "dev"
+  cidr = "10.0.0.0/16"
+ 
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+ 
+  tags = {
+    Project     = "ercot"
+    Owner       = "tuan"
+    Environment = "Dev"
+    Terraform   = "true"
+  }
+ 
 }
